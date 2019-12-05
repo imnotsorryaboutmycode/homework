703b30c092d3e6f10903f1ad39ada6feda791e6d
imnotsorryaboutmycode <46790209+imnotsorryaboutmycode@users.noreply.github.com>
2019-12-05 22:09:05 +0300

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,1 +1,20 @@
-homework #1 py
+'homework #1 py'
+
+'task 1'
+
+var1 = 123
+str1 = 'TEST OUTPUT'
+
+print (var1)
+print (str1)
+
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print ("Hello "+name+" "+surname+" "+str(age)+" old and "+str(height)+" tall :)")
+
+
+'task 2'
